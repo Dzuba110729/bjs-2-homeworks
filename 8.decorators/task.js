16bfb4d4897d6741d5f86480bb357831d84e1d8f
dzuba110729 <dzyuba1107@outlook.com>
2022-07-05 16:45:03 +0300

    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,55 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return function wrapper(...rest) {
+    const key = rest.join(",");
+    let index = cache.findIndex((element) => element.key === key);
+    if (index !== -1) {
+      console.log("Из кэша: " + cache[index].value);
+      return "Из кэша: " + cache[index].value;
+    }
+    let result = func(...rest);
+    cache.push({ key: key, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timeout;
+  let flag = true;
+  return function wrapper(...rest) {
+    if (flag) {
+      func(...rest);
+      flag = false;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...rest);
+      flag = true;
+    }, ms);
+  };
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timeout;
+  let flag = true;
+  function wrapper(...rest) {
+    wrapper.count += 1;
+    if (flag) {
+      func(...rest);
+      flag = false;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...rest);
+      flag = true;
+    }, ms);
+  }
+  wrapper.count = 0;
+  wrapper.getCount = () => console.log("Количество вызовов: " + wrapper.count);
+  return wrapper;
 }
